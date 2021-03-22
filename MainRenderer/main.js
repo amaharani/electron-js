ffca4b6b404817a2e8638f6cd9b0b1b2598b3cec
amaharani <amaharani677@gmail.com>
2021-03-22 19:50:02 +0800

    Tutorial 3 - Main and Renderer Process

---
+++ MainRenderer/main.js
@@ -0,0 +1,58 @@
+console.log('main process working');
+console.log('main.js');
+
+//const {app, BrowserWindow} = require('electron');
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+
+let winOne, winTwo;
+
+function createWindow(){
+    winOne = new BrowserWindow({
+        webPreference:{
+            nativeWindowOpen: true,
+            nodeIntegration: true,
+            enableRemoteModule: true,
+//            contextIsolation: true,
+        }
+    });
+    winTwo = new BrowserWindow();
+
+    winOne.loadURL(url.format({
+        pathname: path.join(__dirname, 'one.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    winTwo.loadURL(url.format({
+        pathname: path.join(__dirname, 'two.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    winOne.webContents.openDevTools();
+    winTwo.webContents.openDevTools();
+
+    winOne.on('closed', () => {
+        winOne = null;
+    })
+    winTwo.on('closed', () => {
+        winTwo = null;
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', () => {
+    if (winOne == null || winTwo == null) {
+        createWindow()
+    }
+});
